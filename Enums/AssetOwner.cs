6954e6760cea3a3d21cf8ee7a57638f79143d3d0
Vikram <tbs.vikram@gmail.com>
2025-08-26 16:58:57 +0530

    add enum for new field

---
+++ Enums/AssetOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum AssetOwner
+    {
+        Engineering = 1,
+        [Display(Name = "Field Ops")]
+        FieldOps = 2,
+        Integrity = 3,
+        Policy = 4
+    }
+}
